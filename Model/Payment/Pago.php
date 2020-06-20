128f984f64db49306a541cb15b2e682a70cda452
Nilton <nilton.rojas@culqi.com>
2020-06-19 21:39:00 -0500

    fixed value

--- Model/Payment/Pago.php
+++ Model/Payment/Pago.php
@@ -7,7 +7,6 @@ class Pago extends \Magento\Payment\Model\Method\AbstractMethod
 {
 
     protected $_code = "culqi";
-    protected $_isOffline = true;
 
     public function isAvailable(
         \Magento\Quote\Api\Data\CartInterface $quote = null
